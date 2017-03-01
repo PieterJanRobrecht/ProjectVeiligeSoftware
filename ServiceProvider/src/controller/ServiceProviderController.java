c89201903160013991afa523f735232796912f9b
Pieter-Jan Robrecht <pieterjan.robrecht@student.kuleuven.be>
2017-03-01 16:03:02 +0100

    Main voor GUI control + init van GUI zelf

--- ServiceProvider/src/controller/ServiceProviderController.java
+++ ServiceProvider/src/controller/ServiceProviderController.java
@@ -9,39 +9,60 @@ import javafx.scene.layout.HBox;
 
 public class ServiceProviderController {
 
-    @FXML
-    private CheckBox adressCheck;
+	@FXML
+	private CheckBox adressCheck;
 
-    @FXML
-    private CheckBox fotoCheck;
+	@FXML
+	private CheckBox fotoCheck;
 
-    @FXML
-    private TextArea communicationArea;
+	@FXML
+	private TextArea communicationArea;
 
-    @FXML
-    private HBox submitButton;
+	@FXML
+	private HBox submitButton;
 
-    @FXML
-    private ComboBox<String> providerCombo;
+	@FXML
+	private ComboBox<String> providerCombo;
 
-    @FXML
-    private CheckBox nameCheck;
+	@FXML
+	private CheckBox nameCheck;
 
-    @FXML
-    private CheckBox ageCheck;
+	@FXML
+	private CheckBox ageCheck;
 
-    @FXML
-    private CheckBox countryCheck;
+	@FXML
+	private CheckBox countryCheck;
 
-    @FXML
-    private CheckBox genderCheck;
+	@FXML
+	private CheckBox genderCheck;
 
-    @FXML
-    private CheckBox birthDateCheck;
+	@FXML
+	private CheckBox birthDateCheck;
 
-    @FXML
-    void submitSettings(ActionEvent event) {
+	@FXML
+	void submitSettings(ActionEvent event) {
+		boolean name = nameCheck.selectedProperty().getValue();
+		boolean adress = adressCheck.selectedProperty().getValue();
+		boolean foto = fotoCheck.selectedProperty().getValue();
+		boolean age = ageCheck.selectedProperty().getValue();
+		boolean country = countryCheck.selectedProperty().getValue();
+		boolean gender = genderCheck.selectedProperty().getValue();
+		boolean birthday = birthDateCheck.selectedProperty().getValue();
+		String output = providerCombo.getSelectionModel().getSelectedItem().toString();
+		String submit = "Selected: " + name + "," + adress + "," + foto + "," + "age" + "," + country + "," + birthday
+				+ " for " + output;
+		addText(submit);
+	}
 
-    }
+	@FXML
+	public void initialize() {
+		providerCombo.getItems().addAll("Overheid 1", "Overheid 2", "Sociaal Netwerk 1", "Sociaal Netwerk 2",
+				"Default 1", "Default 2", "Keuze 1", "Keuze 2");
+		providerCombo.getSelectionModel().selectFirst();
+	}
+
+	public void addText(String text) {
+		communicationArea.appendText(text+"\n");
+	}
 
 }
---
+++ ServiceProvider/src/main/ServiceProviderMain.java
@@ -0,0 +1,39 @@
+package main;
+
+import java.io.IOException;
+
+import controller.ServiceProviderController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ServiceProviderMain extends Application{
+
+	public static void main(String[] args){
+		launch(args);
+	}
+	
+	 public void start(Stage primaryStage) {
+	        try {
+	            //Laden van de fxml file waarin alle gui elementen zitten
+	            FXMLLoader loader = new FXMLLoader();
+	            Parent root = FXMLLoader.load(getClass().getResource("../ServiceProviderGui.fxml"));
+
+	            //Setten van enkele elementen van het hoofdscherm
+	            primaryStage.setTitle("Service Provider");
+	            primaryStage.setScene(new Scene(root));
+	            primaryStage.show();
+
+	            //Ophalen van de controller horende bij de view klasse
+	            ServiceProviderController serviceProviderController = loader.<ServiceProviderController>getController();
+	            assert (serviceProviderController != null);
+
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+
+}
