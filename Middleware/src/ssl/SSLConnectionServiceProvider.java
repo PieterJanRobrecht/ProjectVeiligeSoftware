7c2c4931c69f883484b9c1dfe54a483573566ee4
Tech2Logix <rhinovanboxelaere@outlook.com>
2017-03-29 19:45:42 +0200

    FUCK WINDOWS DEFENDER

--- Middleware/src/ssl/SSLConnectionServiceProvider.java
+++ Middleware/src/ssl/SSLConnectionServiceProvider.java
@@ -27,7 +27,7 @@ import java.util.concurrent.LinkedBlockingQueue;
  * @author rhino
  *
  */
-public class SSLConnectionServiceProvider extends Communicator implements Runnable {
+public class SSLConnectionServiceProvider extends Communicator {
 	SSLSocketFactory sslSocketFactory;
 	SSLSocket sslSocket;
 	MiddlewareController mwc;
@@ -93,62 +93,4 @@ public class SSLConnectionServiceProvider extends Communicator implements Runnab
 			e.printStackTrace();
 		}
 	}
-
-//	public byte[] fetchCert() {
-//		byte[] returnValue = null;
-//		try {
-//			sslSocket = (SSLSocket) sslSocketFactory.createSocket("localhost", 1338);
-//			sslSocket.startHandshake();
-//
-//			InputStream inputStream = sslSocket.getInputStream();
-//			OutputStream outputStream = sslSocket.getOutputStream();
-//
-//			send("0", outputStream);
-//			String cert = null;
-//			for (int i = 0; i < 9; i++) {
-//				cert += receive(inputStream);
-//			}
-//
-//			cert = cert.split("null")[1];
-//
-//			byte[] certInBytes = hexStringToByteArray(cert);
-//
-//			System.out.println("Cert in hex: " + cert);
-//			System.out.println("Cert: " + Arrays.toString(certInBytes));
-//
-//			inputStream.close();
-//			outputStream.close();
-//
-//			returnValue = certInBytes;
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		} finally {
-//			if (sslSocket != null) {
-//				try {
-//					sslSocket.close();
-//				} catch (IOException e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		}
-//
-//		return returnValue;
-//	}
-
-	public static byte[] hexStringToByteArray(String s) {
-		int len = s.length();
-		byte[] data = new byte[len / 2];
-		for (int i = 0; i < len; i += 2) {
-			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
-		}
-		return data;
-	}
-
-	@Override
-	public void run() {
-
-	}
-
 }
